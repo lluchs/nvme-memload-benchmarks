e2fb522a273dee4722895ed97fd89f5c9e1b7ab5
Lukas Werling <lukas.werling@gmail.com>
2015-06-30 14:21:32 +0200

    Add scatter and violin graphs without averaging

--- csv_to_graph.jl
+++ csv_to_graph.jl
@@ -15,11 +15,13 @@ averaged = by(input, [:benchmark, :loadconfig]) do df
 	DataFrame(real = mean(df[:real]), user = mean(df[:user]), sys = mean(df[:sys]))
 end
 
-writetable("$outdir/averaged.csv", averaged)
+writetable("$outdir/average.csv", averaged)
 
-# Draw a ploc containing absolute times.
-fullplot = plot(averaged, xgroup=:benchmark, x=:loadconfig, y=:real, Geom.subplot_grid(Geom.bar))
-draw(SVGJS("$outdir/fullplot.svg", 30cm, 15cm), fullplot)
+pl = plot(input, xgroup=:benchmark, x=:loadconfig, y=:real, color=:loadconfig, Geom.subplot_grid(Geom.point))
+draw(SVGJS("$outdir/scatter.svg", 30cm, 15cm), pl)
 
-fullplot = plot(averaged, x=:benchmark, color=:loadconfig, y=:real, Geom.bar(position=:dodge))
-draw(SVGJS("$outdir/fullplot2.svg", 30cm, 15cm), fullplot)
+pl = plot(input, xgroup=:benchmark, x=:loadconfig, y=:real, Geom.subplot_grid(Geom.violin))
+draw(SVGJS("$outdir/violin.svg", 30cm, 15cm), pl)
+
+pl = plot(averaged, x=:benchmark, color=:loadconfig, y=:real, Geom.bar(position=:dodge))
+draw(SVGJS("$outdir/average.svg", 30cm, 15cm), pl)
