cdb05d6b3c8ea5af3d8d66b797c5d728fef6177f
Lukas Werling <lukas.werling@gmail.com>
2015-07-13 23:14:34 +0200

    Fix missing make step for silo setup

--- perfkit/setup.sh
+++ perfkit/setup.sh
@@ -6,5 +6,6 @@ mkdir -p benchmarks
     git clone https://github.com/stephentu/silo.git benchmarks/silo
     cd benchmarks/silo
     git checkout cc11ca1ea949ef266ee12a9b1c310392519d9e3b
+    make -j
     make -j dbtest
 )
