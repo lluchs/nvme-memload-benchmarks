c725b12436e1f0cb729091ef2b84224175d30b92
Lukas Werling <lukas.werling@gmail.com>
2015-07-13 11:50:35 +0200

    Add setup script for silo benchmark

---
+++ perfkit/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p benchmarks
+
+[[ -d benchmarks/silo ]] || (
+    git clone https://github.com/stephentu/silo.git benchmarks/silo
+    cd benchmarks/silo
+    git checkout cc11ca1ea949ef266ee12a9b1c310392519d9e3b
+    make -j dbtest
+)
