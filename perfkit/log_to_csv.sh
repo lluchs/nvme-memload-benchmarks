cab13a2c51c8a24b59466273055d37c7fcbdf235
Lukas Werling <lukas.werling@gmail.com>
2015-07-22 15:57:29 +0200

    Add runtime and throughput information for the memtier benchmark

--- perfkit/log_to_csv.sh
+++ perfkit/log_to_csv.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Assume all headers are the same. Print only the first one.
-sed -n '/^🕑HEAD,/{s///;s/$/,runtime,agg_throughput/;p;q}' <&0
+sed -n '/^🕑HEAD,/{s///;s/$/,runtime,throughput/;p;q}' <&0
 # Print all content rows.
 sed -n '
+# Data reported by the silo benchmarks.
 /^runtime: \(.*\) sec$/ {
     s//,\1/
     h
@@ -12,6 +13,18 @@ sed -n '
     s//,\1/
     H
 }
+
+# Data reported by the memtier benchmark.
+/.*\[RUN #1 100%,\s*\(\S*\) secs\].*/ {
+    s//,\1/
+    h
+}
+/^Totals    \(\S*\) .*$/ {
+    s//,\1/
+    H
+}
+
+# Benchmark done, print the timing row.
 /^🕑,/ {
     G
     s///
