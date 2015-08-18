95b2ca3e4d602321f5d673f6a7f8aaa262210f16
Lukas Werling <lukas.werling@gmail.com>
2015-08-18 14:14:28 +0200

    Improve PCM benchmark scripts

--- pcm/run.sh
+++ pcm/run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-COUNTERS='PCIeRdCur-hits PCIeRdCur-misses RFO-hits-filtered RFO-misses-filtered ItoM-hits-filtered ItoM-misses-filtered'
-#PATTERN=flush
-#LIMIT=-t5
+# Optional parameter
+${COUNTERS:='PCIeRdCur-hits PCIeRdCur-misses RFO-hits-filtered RFO-misses-filtered ItoM-hits-filtered ItoM-misses-filtered'}
+
+# Required parameters
+${NAME:?}
+${PATTERN:?}
+${LIMIT:?}
+
+sudo nvme format /dev/nvme0n1 -l3
 
 for counter in $COUNTERS; do
-	echo "→ $counter with $PATTERN"
+	echo "→ $counter with $NAME"
 	sudo LD_LIBRARY_PATH=. $(which nvme-memload) -p$counter $LIMIT /dev/nvme0n1 $PATTERN
 done
--- pcm/run_multiple.sh
+++ pcm/run_multiple.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
 
+export NAME
 export PATTERN
 export LIMIT
 
+NAME=single-write
 PATTERN=single
 LIMIT=-G500000
 ./run.sh
 
+NAME=single-read
+PATTERN="single -oread"
+LIMIT=-G250000
+./run.sh
+
+NAME=random-write
 PATTERN=random
 LIMIT=-G100000
 ./run.sh
 
+NAME=random-read
+LIMIT=-G50000
+PATTERN="random -oread"
+./run.sh
+
+NAME=flush
 PATTERN=flush
 LIMIT=-G350000
 ./run.sh
 
+# Run noop with all available counters.
+export COUNTERS='PCIeRdCur-hits PCIeRdCur-misses RFO-hits-filtered RFO-misses-filtered ItoM-hits-filtered ItoM-misses-filtered CRd-hits CRd-misses DRd-hits DRd-misses PRd-hits PRd-misses WiL-hits WiL-misses'
+NAME=noop
 PATTERN=noop
 LIMIT=-t5
 ./run.sh
